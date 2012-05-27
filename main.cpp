cc95dedd746d1269221610dcadd47a4d4db7542b
Farley Matos <farleyfmatos@gmail.com>
2012-05-27 19:12:53 -0300

    Programa Principal (Interface como usario)

---
+++ main.cpp
@@ -0,0 +1,60 @@
+/*
+  TRABALHO DE LINGUAGENS DE PROGRAMAÇÃO
+  Calculadora em linha de comando 
+              -Linguagem C++
+              -Modularizada
+              -Compartilhada pelo GitHub
+  
+  Professor: Leonardo Nunes
+  Alunos: Bruno saraiva
+          Farley Matos
+          Laura Panzariello
+          Stephanie Cancado
+          Timon  Asch
+          
+  Modulo Principal - Interface com o Usuário
+*/
+
+
+#include <cstdio>
+#include <iostream>
+#include <cstdlib>
+#include <string>
+#include <ctype.h>
+
+#include "mod_cal.h"
+
+using namespace std;
+
+
+int main(int argc, char *argv[])
+{
+	string expr;
+
+	puts("Digite a expressao desejada e tecle enter");
+	puts("Digite 'sair' para terminar o programa");
+
+    
+INICIO:
+        try
+        {
+        
+        	do
+        	{               
+        		printf("> ");
+        		cin >> expr;
+        		if (expr == "sair") break;
+        		else parse(expr);
+        		
+        		for(int i=0;i<100000;i++); // Para lerdar o computador. Porque eh rapido demais.
+        		
+        	} while (1);
+        
+        }
+        catch (int e)
+        {
+             goto INICIO;
+        }
+
+	return 1;
+}
