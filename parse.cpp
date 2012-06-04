ef37e46f466516d3a82f4acbae50879d221a5f59
Farley Matos <farleyfmatos@gmail.com>
2012-06-03 21:30:21 -0300

    Modulo Parse

--- parse.cpp
+++ parse.cpp
@@ -12,7 +12,8 @@
           Stephanie Cancado
           Timon  Asch
           
-  Modulo Parse 
+  Modulo Parse:
+         Separa a expressao inserida pelo usuario em tokens, para passa-los pela parse-tree.
 */
 
 
@@ -37,57 +38,61 @@ int parse(string expr)
       // quebra o texto de entrada em tokens
       for (int i =0; i < expr.size() ; i++)
       {
+          
           //separador espaço
           if(expr_c[i] == ' ')
-          {
+          {               //se tem algo no buffer
                        if(buffer != NULL)
-                       {
+                       {            //e eh um '.' sozinho == erro
                                  if(!strcmp(buffer,"."))
                                  {
                                       cout << "ERRO - REDIGITE A EXPRESSAO" << endl; 
                                       throw 1; //jogar um erro do tipo 1 - o catch vai receber 1 na variavel e
                                  }
-                                 token_count++;
+                                 token_count++; //se esta tudo ok, entao fechamos um token
+                                 
+                                    //se eh o primeiro token que encontramos, precisamos alocar a memoria para o "token" e para o "token_type"
                                  if (token_count == 1) 
                                  {
                                                  token = (char **) malloc(sizeof(char *));
                                                  token_type = (char *) malloc(sizeof(char));
                                  }
-                                 else
+                                 else            //senao, apenas realocamos
                                  {
                                                  token = (char **) realloc(token,token_count*sizeof(char *));
                                                  token_type = (char *) realloc(token_type,token_count*sizeof(char));
                                  }
                                  
-                                 token[token_count-1] = (char *) malloc(strlen(buffer)*sizeof(char));
-                                 strcpy(token[token_count-1],buffer);
-                                 token_type[token_count-1] = tipo_buffer;
-
+                                 token[token_count-1] = (char *) malloc(strlen(buffer)*sizeof(char));  //alocando memoria para o "*token"
+                                 strcpy(token[token_count-1],buffer);  //armazenando o token no "vetor de tokens"
+                                 token_type[token_count-1] = tipo_buffer;  //armazenando o tipo do token no "vetor de tipos de token"
+                                 
+                                    //resetando o buffer, para procurarmos outro token
                                  free(buffer);
                                  buffer = NULL;
                                  tipo_buffer = -1;
-                                 
-                                 //cout << "TOKEN [" << token[token_count-1] << "] - " << token_type[token_count-1] << " - " << token_count << endl;
                        }
           }
           
-          //separador demais
+          //separador de sinais
           else if(expr_c[i] == '(' || expr_c[i] == ')' || expr_c[i] == '+' || expr_c[i] == '-' || expr_c[i] == '*' || expr_c[i] == '/' || expr_c[i] == ';')
-          {
+          {               //se tem algo no buffer
                        if(buffer != NULL)
-                       {         
+                       {            //e eh um '.' sozinho == erro
                                  if(!strcmp(buffer,"."))
                                  {
                                       cout << "ERRO - REDIGITE A EXPRESSAO" << endl; 
                                       throw 1;
                                  }    
-                                 token_count++;
+                                 token_count++;  //se esta tudo ok, entao fechamos um token
+                                 
+                                    //se eh o primeiro token que encontramos, precisamos alocar a memoria para o "token" e para o "token_type"
                                  if (token_count == 1) 
                                  {
                                                  token = (char **) malloc(sizeof(char *));
                                                  token_type = (char *) malloc(sizeof(char));
                                  }
-                                 else
+                                 else            //senao, apenas realocamos
                                  {
                                                  token = (char **) realloc(token,token_count*sizeof(char *));
                                                  token_type = (char *) realloc(token_type,token_count*sizeof(char));
@@ -97,12 +102,13 @@ int parse(string expr)
                                  strcpy(token[token_count-1],buffer);
                                  token_type[token_count-1] = tipo_buffer;
 
+                                    //resetando o buffer, para procurarmos outro token
                                  free(buffer);
                                  buffer = NULL;
                                  tipo_buffer = -1;
-                                 
-                                 //cout << "TOKEN [" << token[token_count-1] << "] - " << token_type[token_count-1] << " - " << token_count << endl;
                        }
+                       
+                          //armazenando os sinais no "vetor de tokens"
                        token_count++;
                        if (token_count == 1) 
                        {
@@ -119,30 +125,30 @@ int parse(string expr)
                        token[token_count-1] = (char *) malloc(2*sizeof(char));
                        token[token_count-1][0] = expr_c[i];
                        token[token_count-1][1] = '\0';
-                       token_type[token_count-1] = 'S';
-                        
-                       //cout << "TOKEN [" << token[token_count-1] << "] - " << token_type[token_count-1] << " - " << token_count << endl;
+                       token_type[token_count-1] = 'S';  //tipo "sinal"
           }
           
           //separador :=
-          else if(expr_c[i] == ':' && expr.size() != (i+1))
-          {
-                       if(expr_c[i+1] == '=')
-                       {
+          else if(expr_c[i] == ':' && expr.size() != (i+1))  //se eh ':'
+          {                 
+                       if(expr_c[i+1] == '=')  //e depois vem '=', entao ok
+                       {         //se tem algo no buffer
                               if(buffer != NULL)
-                               {    
+                               {            //e eh um '.' sozinho == erro
                                          if(!strcmp(buffer,"."))
                                          {
                                               cout << "ERRO - REDIGITE A EXPRESSAO" << endl; 
                                               throw 1;
                                          }
-                                         token_count++;
+                                         token_count++;  //se esta tudo ok, entao fechamos um token
+                                         
+                                            //se eh o primeiro token que encontramos, precisamos alocar a memoria para o "token" e para o "token_type"
                                          if (token_count == 1) 
                                          {
                                                          token = (char **) malloc(sizeof(char *));
                                                          token_type = (char *) malloc(sizeof(char));
                                          }
-                                         else
+                                         else            //senao, apenas realocamos
                                          {
                                                          token = (char **) realloc(token,token_count*sizeof(char *));
                                                          token_type = (char *) realloc(token_type,token_count*sizeof(char));
@@ -152,13 +158,13 @@ int parse(string expr)
                                          strcpy(token[token_count-1],buffer);
                                          token_type[token_count-1] = tipo_buffer;
         
+                                            //resetando o buffer, para procurarmos outro token
                                          free(buffer);
                                          buffer = NULL;
                                          tipo_buffer = -1;
-                                         
-                                         //cout << "TOKEN [" << token[token_count-1] << "] - " << token_type[token_count-1] << " - " << token_count << endl;
                                }  
                                
+                                  //armazenando o simbolo de atribuicao no "vetor de tokens"
                                token_count++;
                                if (token_count == 1) 
                                {
@@ -175,63 +181,69 @@ int parse(string expr)
                                token[token_count-1] = (char *) malloc(2*sizeof(char));
                                token[token_count-1][0] = '=';
                                token[token_count-1][1] = '\0';
-                               token_type[token_count-1] = 'A';
-                                
-                               //cout << "TOKEN [" << token[token_count-1] << "] - " << token_type[token_count-1] << " - " << token_count << endl;  
+                               token_type[token_count-1] = 'A';  //tipo "atribuicao"
                                
-                               i++;    
+                               i++;  //incrementa o i para pular o caracter '=' que vem depois do ':' 
                        }
-                       else
+                       else  //senao == erro
                        {
                            cout << "ERRO - REDIGITE A EXPRESSAO" << endl;
                            throw 1;
                        }
           }
+          
           // monta variaveis e numeros
           else if(isalnum(expr_c[i]) || expr_c[i] == '.')
-          {
+          {                       //se encontrou algo que nao eh sinal, entao eh variavel ou numero
                               if(buffer == NULL)
                               {
                                          buffer = (char *) malloc(2*sizeof(char));
-                                         buffer[1] = '\0'; 
+                                         buffer[1] = '\0';
                                          buffer[0] =  expr_c[i];
-                                         if (isalpha(expr_c[i])) tipo_buffer = 'V';
-                                         else if (expr_c[i] == '.') tipo_buffer = 'D';
-                                         else tipo_buffer = 'I';
+                                         if (isalpha(expr_c[i])) tipo_buffer = 'V';  //eh uma variavel
+                                         else if (expr_c[i] == '.') tipo_buffer = 'D';  //se tiver o caracter '.', eh um decimal
+                                         else tipo_buffer = 'I';  //eh um int
                               }
+                              //a variavel/numero pode ter mais de um caracter, entao o buffer soh fecha quando encontrar um sinal/espaco
+                              
+                                  //continuando a montar a variavel/numero
                               else
                               { 
-                                          if(tipo_buffer == 'I' && isalpha(expr_c[i]))
+                                          if(tipo_buffer == 'I' && isalpha(expr_c[i]))  //inteiro seguido de letra == erro
                                           {
                                                          cout << "ERRO - REDIGITE A EXPRESSAO" << endl;
                                                          throw 1;                         
                                           }
-                                          if(tipo_buffer == 'D' && isalpha(expr_c[i]))
+                                          if(tipo_buffer == 'D' && isalpha(expr_c[i]))  //decimal seguido de letra == erro
                                           {
                                                          cout << "ERRO - REDIGITE A EXPRESSAO" << endl;
                                                          throw 1;                        
                                           }
                                            
-                                          if(tipo_buffer == 'V' && expr_c[i] == '.')
+                                          if(tipo_buffer == 'V' && expr_c[i] == '.')  //variavel seguida de '.' == erro
                                           {
                                                          cout << "ERRO - REDIGITE A EXPRESSAO" << endl;
                                                          throw 1;                         
                                           } 
-                                          if(tipo_buffer == 'D' && expr_c[i] == '.')
+                                          if(tipo_buffer == 'D' && expr_c[i] == '.')  //decimal seguido de '.' == erro
                                           {
                                                          cout << "ERRO - REDIGITE A EXPRESSAO" << endl;
                                                          throw 1;                        
                                           } 
-                                          if(tipo_buffer == 'I' && expr_c[i] == '.')
+                                          if(tipo_buffer == 'I' && expr_c[i] == '.')  //inteiro seguido de '.' == decimal
                                           {
                                                          tipo_buffer = 'D';                        
                                           } 
+                                          
+                                            //juntar esta "parte" da variavel/numero com a que ja tinhamos
                                           buffer = (char *) realloc(buffer,(strlen(buffer)+2)*sizeof(char));
                                           buffer[strlen(buffer)+1] = '\0'; 
                                           buffer[strlen(buffer)] =  expr_c[i]; 
                               }    
                                        
           }
+          
+             //se nao for nenhuma dos anteriores == erro
           else
           {
               cout << "ERRO - REDIGITE A EXPRESSAO" << endl;
@@ -241,20 +253,24 @@ int parse(string expr)
           
       }
       
+         //para guardarmos o ultimo token armazenado no buffer
+             //se tem algo no buffer
       if(buffer != NULL)
-      {    
+      {         //e eh um '.' sozinho  == erro
            if(!strcmp(buffer,"."))
            {
                 cout << "ERRO - REDIGITE A EXPRESSAO" << endl; 
                 throw 1;
            }
-           token_count++;
+           token_count++;  //se esta tudo ok, entao fechamos um token
+           
+              //se eh o primeiro token que encontramos, precisamos alocar a memoria para o "token" e para o "token_type"
            if (token_count == 1) 
            {
                            token = (char **) malloc(sizeof(char *));
                            token_type = (char *) malloc(sizeof(char));
            }
-           else
+           else            //senao, apenas realocamos
            {
                            token = (char **) realloc(token,token_count*sizeof(char *));
                            token_type = (char *) realloc(token_type,token_count*sizeof(char));
@@ -264,14 +280,13 @@ int parse(string expr)
            strcpy(token[token_count-1],buffer);
            token_type[token_count-1] = tipo_buffer;
         
+              //resetando o buffer para procurarmos outro token
            free(buffer);
            buffer = NULL;
            tipo_buffer = -1;
-                                         
-        //cout << "TOKEN [" << token[token_count-1] << "] - " << token_type[token_count-1] << " - " << token_count << endl;
-        }
+      }
   
-      parse_tree(token,token_type,token_count);
+      parse_tree(token,token_type,token_count);  //passando o "vetor de tokens" pela parse-tree
       
       return 1;
 }
